dc484bd18056156d67a87329bdd204d26dbd2b2d
LeandroMelloo <leandromoreno1909@hotmail.com>
2022-08-31 23:09:49 -0300

    criação do perfil de DEV

--- src/main/java/com/leandro/bookstore/service/DBService.java
+++ src/main/java/com/leandro/bookstore/service/DBService.java
@@ -21,16 +21,20 @@ public class DBService {
 	public void instanciaBaseDeDados() {
 		Categoria categoria1 = new Categoria(null, "Tecnologia", "Livro de Tecnologia");
 		Categoria categoria2 = new Categoria(null, "Informática", "Livro de Informática");
+		Categoria categoria3 = new Categoria(null, "Biologia", "Livro de Biologia");
 
 		Livro livro1 = new Livro(null, "Clean Code", "Robert Martin", "Livro código limpo", categoria1);
 		Livro livro2 = new Livro(null, "NodeJS", "Robert Martin", "Livro NodeJS", categoria2);
 		Livro livro3 = new Livro(null, "Java", "Robert Martin", "Livro Java", categoria1);
 		Livro livro4 = new Livro(null, "Python", "Robert Martin", "Livro de Python", categoria2);
+		Livro livro5 = new Livro(null, "Planeta Vegetal", "Adiano del Pietro", "Livro Planeta Vegetal", categoria3);
+		Livro livro6 = new Livro(null, "Planeta Terra", "Carlos Albanes", "Planeta Terra para todos", categoria3);
 
 		categoria1.getLivros().addAll(Arrays.asList(livro1, livro3));
 		categoria2.getLivros().addAll(Arrays.asList(livro2, livro4));
+		categoria3.getLivros().addAll(Arrays.asList(livro5, livro6));
 
-		this.categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
-		this.livroRepository.saveAll(Arrays.asList(livro1, livro2, livro3));
+		this.categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2, categoria3));
+		this.livroRepository.saveAll(Arrays.asList(livro1, livro2, livro3, livro4, livro5, livro6));
 	}
 }
